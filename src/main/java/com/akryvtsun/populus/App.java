fbb9bc57f995c14bedd24ec34cfeb1743adcfe42
Andriy Kryvtsun <andy@akryvtsun.com>
2020-01-12 12:12:36 +0200

    open desktop browser

--- src/main/java/com/akryvtsun/populus/App.java
+++ src/main/java/com/akryvtsun/populus/App.java
@@ -1,13 +1,14 @@
 package com.akryvtsun.populus;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class App {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        System.out.println("Hello World!");
+        Desktop.getDesktop().browse(new URI("https://echo.msk.ru/"));
     }
 }
